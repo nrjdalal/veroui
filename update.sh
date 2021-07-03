88eda22347d81b7f2724e96ce4faf9e66a077959
Neeraj Dalal <admin@nrjdalal.com>
2021-07-03 20:49:36 +0530

    update

--- update.sh
+++ update.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 set -e
 
-read -p "Commit Message - " COMMITMSG
-
-git add -A
-git commit -m "$COMMITMSG"
-git push
-
 read -p "Do you wish to publish the package? " RESPONSE
 
 case $RESPONSE in
 yes | y)
   npx @nrjdalal/publish
-  npm publish
   ;;
 *)
-  echo "Exiting w/o publishing!"
+  echo "Skipping publishing!"
   ;;
 esac
+
+read -p "Commit Message - " COMMITMSG
+
+git add -A
+git commit -m "$COMMITMSG"
+git push
