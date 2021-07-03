d98cf6d82cfbe9b7717bd87d08cad78499a9b20e
Neeraj Dalal <admin@nrjdalal.com>
2021-07-03 21:41:15 +0530

    hell yeah

--- bin/react.sh
+++ bin/react.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ./components/veroui/
-cp -r ./node_modules/@veroui/react/src/ ./components/veroui/
+cp -r ./node_modules/@veroui/react/src/* ./components/veroui/
 echo "Successfully added @veroui/react"
