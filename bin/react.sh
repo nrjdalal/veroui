b29ff1a8fd7dcc2894ed6f1a7fa3a2a1e255599b
Neeraj Dalal <admin@nrjdalal.com>
2021-07-03 21:04:45 +0530

    testing

--- bin/react.sh
+++ bin/react.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+curl -fsSL https://raw.githubusercontent.com/nrjdalal/veroui/master/src/veroui.jsx >./components/
 echo "Successfully installed @veroui/react"
--- src/veroui.jsx
+++ src/veroui.jsx
@@ -1,1 +1,7 @@
-// test file
+export const Dummy = () => {
+  return (
+    <>
+      <div className="h-16 w-16 bg-gray-500"></div>
+    </>
+  )
+}
