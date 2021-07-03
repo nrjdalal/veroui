8577aa55f0b118743f70437d66975252642bcd0a
Neeraj Dalal <admin@nrjdalal.com>
2021-07-03 20:54:39 +0530

    nothing new

--- update.sh
+++ update.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 set -e
 
-read -p "Do you wish to publish the package? " RESPONSE
-
-case $RESPONSE in
-yes | y)
-  npx @nrjdalal/publish
-  ;;
-*)
-  echo "Skipping publishing!"
-  ;;
-esac
-
 read -p "Commit Message - " COMMITMSG
+
+npx @nrjdalal/publish
+
+cp ./dev/components/veroui.jsx ./src
 
 git add -A
 git commit -m "$COMMITMSG"
