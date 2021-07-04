d4cbb94f27ceec33426d0b0021f6c8610088ed56
Neeraj Dalal <admin@nrjdalal.com>
2021-07-04 15:18:30 +0530

    v1

--- bin/react.sh
+++ bin/react.sh
@@ -3,27 +3,27 @@
 echo
 echo "[1] Downloading latest components"
 
-# YARN=$(npm list -g yarn | grep -c 'yarn')
+YARN=$(npm list -g yarn | grep -c 'yarn')
 
-# if [[ $YARN -ne 0 ]]; then
-#   yarn add --dev @veroui/react@latest 2>/dev/null >/dev/null
-# else
-#   npm install -D @veroui/react@latest 2>/dev/null >/dev/null
-# fi
+if [[ $YARN -ne 0 ]]; then
+  yarn add --dev @veroui/react@latest 2>/dev/null >/dev/null
+else
+  npm install -D @veroui/react@latest 2>/dev/null >/dev/null
+fi
 
 echo "[2] Replacing existing components"
 
-# mkdir -p ./components/veroui/
-# rm -rf ./components/veroui/*
-# cp -r ./node_modules/@veroui/react/src/* ./components/veroui/
+mkdir -p ./components/veroui/
+rm -rf ./components/veroui/*
+cp -r ./node_modules/@veroui/react/src/* ./components/veroui/
 
 echo "[3] Cleaning up files and folders"
 
-# if [[ $YARN -ne 0 ]]; then
-#   yarn remove @veroui/react 2>/dev/null >/dev/null
-# else
-#   npm uninstall @veroui/react 2>/dev/null >/dev/null
-# fi
+if [[ $YARN -ne 0 ]]; then
+  yarn remove @veroui/react 2>/dev/null >/dev/null
+else
+  npm uninstall @veroui/react 2>/dev/null >/dev/null
+fi
 
 echo
 echo "✅ Successfully added @veroui/react"
