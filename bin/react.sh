2655a2e5238061d62620d42c9e3f2914d95436f2
Neeraj Dalal <admin@nrjdalal.com>
2021-07-04 13:39:41 +0530

    added new script

--- bin/react.sh
+++ bin/react.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-npm i -D @veroui/react@latest
+YARN=$(npm list -g yarn | grep -c 'yarn')
+
+if [[ $YARN -ne 0 ]]; then
+  yarn add --dev @veroui/react@latest
+else
+  npm install -D @veroui/react@latest
+fi
+
 mkdir -p ./components/veroui/
+rm -rf ./components/veroui/*
 cp -r ./node_modules/@veroui/react/src/* ./components/veroui/
+
+echo
 echo "Successfully added @veroui/react"
